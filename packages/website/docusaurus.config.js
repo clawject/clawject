a1b1d5125525436fb61f1196c2e1f0b4027130d5
Artem Kornev <artemkakornev1@gmail.com>
2023-06-23 21:17:30 +0300

    update deploy_website.yml

--- packages/website/docusaurus.config.js
+++ packages/website/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Clawject',
+  title: 'Clawject - Dependency Injection for TypeScript',
   tagline: 'TypeScript DI made easy',
   favicon: 'img/favicon.ico',
 
