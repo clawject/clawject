0ffe634b66e6658272304ecd442b0b8dfc673236
Artem Kornev <artemkakornev1@gmail.com>
2023-09-02 21:56:35 +0300

    Docs

--- packages/clawject/src/runtime/object-factory/ObjectFactory.ts
+++ packages/clawject/src/runtime/object-factory/ObjectFactory.ts
@@ -1,7 +1,13 @@
 /** @public */
 export type ObjectFactoryResult = string | number | boolean | bigint | Symbol | object;
 
-/** @public */
+/**
+ * Defines a factory which can return an Object instance when invoked.
+ *
+ * @see CustomScope
+ *
+ * @public
+ */
 export interface ObjectFactory {
   getObject(): ObjectFactoryResult;
 }
