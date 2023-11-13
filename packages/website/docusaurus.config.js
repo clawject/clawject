6330547c490b3bac8f3fafc0d5d533475d4dfa18
Artem Kornev <artemkakornev1@gmail.com>
2023-11-13 10:49:31 +0200

    Migrate to docusaurus 3

--- packages/website/docusaurus.config.js
+++ packages/website/docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -142,8 +143,9 @@ const config = {
                 // copyright: `Made with . Built with Docusaurus.`,
             },
             prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
+                theme: lightTheme,
+                darkTheme: darkTheme,
+                additionalLanguages: ['typescript', "javascript", 'json', "bash"],
                 magicComments: [
                     {
                         className: 'theme-code-block-highlighted-line',
