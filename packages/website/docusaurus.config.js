46ef95c8b3d9fdaa3fb5ffe551e545edae53b3af
Artem Kornev <artemkakornev1@gmail.com>
2023-11-17 13:30:55 +0200

    Docs in progress

--- packages/website/docusaurus.config.js
+++ packages/website/docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
                 items: [
                     {
                         type: 'docSidebar',
-                        sidebarId: 'tutorialSidebar',
+                        sidebarId: 'docs',
                         position: 'left',
                         label: 'Docs',
                     },
