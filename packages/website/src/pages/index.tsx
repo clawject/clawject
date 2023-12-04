c9028611c8ee7cee10193e4cd6325f59d914dbf2
Artem Kornev <artemkakornev1@gmail.com>
2023-12-04 21:04:28 +0200

    Docs

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -1,28 +1,53 @@
 import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import LogoBig from '@site/static/img/logo-big.svg';
-
+import classNames from 'classnames';
+import Link from '@docusaurus/Link';
 import styles from './index.module.css';
+import { TypeAnimation } from 'react-type-animation';
 
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-
-  return (
-    <div className={styles.homepageHeader}>
-
-    </div>
-  );
-}
+const WORDS = [
+  'Declarative',
+  'Intuitive',
+  'External',
+  'Lightweight',
+  'Adaptive',
+  'EasyToUse',
+  'Visionary',
+  'Forward-looking',
+  'Resourceful',
+  'Far-sighted',
+  'Well-prepared',
+  'Forward-planning',
+  'Prophetic',
+  'Sagacious',
+  'Innovative',
+].map(it => [it, 2000]).flat();
 
 export default function Home(): JSX.Element {
   return (
     <Layout
       description="TypeScript Dependency Injection Framework"
     >
-      <HomepageHeader/>
       <main>
+        <div className={classNames('hero', styles.hero)}>
+
+          <div className={styles.contentContainer}>
+            <h1 className={classNames('hero__title')}>Clawject</h1>
+            <p className="hero__subtitle">TypeScript powered DI framework</p>
+            <TypeAnimation sequence={WORDS} speed={30} repeat={Infinity} className="hero__subtitle" />
+            <Link to="/docs">
+              <button className="button button--primary button--outline button--lg">
+                  Get Started
+              </button>
+            </Link>
+          </div>
+
+          <div className={classNames(styles.logoContainer)}>
+            <div className={styles.logoBackground} />
+            <img className={classNames(styles.logo)} src="/img/logo.svg" alt="Clawject" />
+          </div>
+        </div>
+
       </main>
     </Layout>
   );
