4463b04dadfcbe04574422bbeb8d5b11865a9e75
Artem Kornev <artemkakornev1@gmail.com>
2024-02-22 22:17:00 +0200

    Update documentation

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -42,31 +42,12 @@ const SUBTITLE_PHRASES = [
 ];
 
 export default function Home(): JSX.Element {
-  const subtitle = React.useMemo(() => sample(SUBTITLE_PHRASES), []);
 
   return (
     <Layout
       description="TypeScript Dependency Injection Framework"
     >
-      <div className={classNames('hero', styles.heroContainer)}>
-
-        <div className={styles.contentContainer}>
-          <h1 className={classNames('hero__title')}>Clawject</h1>
-          <p className={classNames('hero__subtitle', styles.heroSubtitle)}>
-            {subtitle}
-          </p>
-          <TypeAnimation preRenderFirstString sequence={WORDS} speed={10} repeat={Infinity}
-            className={styles.typeAnimation}/>
-          <Link className={classNames('button button--primary button--outline button--lg')} to="/docs">
-              Get Started
-          </Link>
-        </div>
-
-        <div className={classNames(styles.logoContainer, 'margin-top--lg')}>
-          <div className={styles.logoBackground}/>
-          <img className={classNames(styles.logo)} src="/img/logo.svg" alt="Clawject"/>
-        </div>
-      </div>
+      123
     </Layout>
   );
 }
