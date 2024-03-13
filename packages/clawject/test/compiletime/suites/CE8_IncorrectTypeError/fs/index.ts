16e401414e152aacba7f88ce2a3215083f2b2d3a
Artem Kornev <artemkakornev1@gmail.com>
2024-03-13 19:59:43 +0200

    Fix test suite

--- packages/clawject/test/compiletime/suites/CE8_IncorrectTypeError/fs/index.ts
+++ packages/clawject/test/compiletime/suites/CE8_IncorrectTypeError/fs/index.ts
@@ -6,17 +6,14 @@ export class Application {
   @Bean voidValueExpression: void = undefined;
   @Bean nullValueExpression: null = null;
   @Bean neverValueExpression: never = undefined as never;
-  @Bean unionValueExpression: string | number = 'string';
 
   @Bean undefinedFactoryMethod(): undefined { return undefined; }
   @Bean voidFactoryMethod(): void { return undefined; }
   @Bean nullFactoryMethod(): null { return null; }
   @Bean neverFactoryMethod(): never { throw new Error('never'); }
-  @Bean unionFactoryMethod(): string | number { return 'string'; }
 
   @Bean undefinedFactoryArrowFunction = (): undefined => { return undefined; };
   @Bean voidFactoryArrowFunction = (): void => { return undefined; };
   @Bean nullFactoryArrowFunction = (): null => { return null; };
   @Bean neverFactoryArrowFunction = (): never => { throw new Error('never'); };
-  @Bean unionFactoryArrowFunction = (): string | number => { return 'string'; };
 }
