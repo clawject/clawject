da051b42ecd00875e037ca86ff39cb7508ea104e
Artem Korniev <artemkakornev1@gmail.com>
2024-08-14 12:58:27 +0300

    fix(di): add default value for tsconfig path

--- packages/di/src/unplugin/types.ts
+++ packages/di/src/unplugin/types.ts
@@ -6,20 +6,23 @@ type FilterPattern = ReadonlyArray<string | RegExp> | string | RegExp | null;
 export type Options = {
   /**
    * The patterns of files to include.
-   * Default: [/\.[cm]?tsx?$/]
+   *
+   * `Default`: `[/\.[cm]?tsx?$/]`
    */
   include?: FilterPattern;
 
   /**
    * The patterns of files to exclude.
-   * Default: [/node_modules/]
+   *
+   * `Default`: `[/node_modules/]`
    */
   exclude?: FilterPattern;
 
   /**
    * The path to the tsconfig file.
    * If not specified, the plugin will try to find it automatically.
-   * Default: undefined
+   *
+   * `Default`: `tsconfig.json`
    */
   tsconfig?: string;
 
@@ -31,7 +34,7 @@ export type Options = {
    * to skip the additional compilation by this plugin
    * and utilize clawject transformer as a typescript compiler plugin https://clawject.com/docs/setup#bundler-with-typescript-loader
    *
-   * Default: false
+   * `Default`: `false`
    */
   skipCompilation?: boolean;
 }
@@ -39,7 +42,8 @@ export type Options = {
 const defaultOptions: Options = {
   include: [/\.[cm]?tsx?$/],
   exclude: [/node_modules/],
-  skipCompilation: false
+  skipCompilation: false,
+  tsconfig: 'tsconfig.json'
 };
 
 export const resolveOptions = (options: Partial<Options> = {}): Options => {
