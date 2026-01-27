7c998e22ce577088c1dc34dbc8286b208e723461
Artem Korniev <artemkakornev1@gmail.com>
2026-01-27 11:44:16 +0200

    fix: bump ts 5.9 (#22)

--- packages/core/src/constants/index.ts
+++ packages/core/src/constants/index.ts
@@ -1,7 +1,7 @@
 import upath from 'upath';
 
 /** this line is parsed by the build script */
-const TS_VERSION_RANGE = '>=5.0 <=5.7';
+const TS_VERSION_RANGE = '>=5.0 <=5.9';
 
 export const CONSTANTS = {
   libraryName: '@clawject/di',
--- packages/di/src/unplugin/core/createTSMorphProject.ts
+++ packages/di/src/unplugin/core/createTSMorphProject.ts
@@ -8,8 +8,10 @@ const ImportMap = {
   '5.3': 'ts-morph-bootstrap-ts53',
   '5.4': 'ts-morph-bootstrap-ts54',
   '5.5': 'ts-morph-bootstrap-ts55',
-  '5.6': 'ts-morph-bootstrap-ts56-57',
-  '5.7': 'ts-morph-bootstrap-ts56-57',
+  '5.6': 'ts-morph-bootstrap-ts56',
+  '5.7': 'ts-morph-bootstrap-ts57',
+  '5.8': 'ts-morph-bootstrap-ts58',
+  '5.9': 'ts-morph-bootstrap-ts59',
 };
 
 export const createTSMorphProject: typeof createProject = async (...args: any[]) => {
